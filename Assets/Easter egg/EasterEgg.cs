a4fe71c871be04884e9f210c55ae94649a4b6818
Patrick Hogenboom <patrickhightree@gmail.com>
2019-04-07 23:06:49 +0200

    Cleanup

--- Assets/Easter egg/EasterEgg.cs
+++ Assets/Easter egg/EasterEgg.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -18,81 +17,121 @@ public class EasterEgg : MonoBehaviour
     private List<Renderer> m_renderers;
     private VRTK_InteractableObject m_interactableObject;
     private VRTK_ControllerReference m_controllerReference;
+    private static readonly int Blend = Shader.PropertyToID("_EffectBlend");
+    private static readonly int NumberSteps = Shader.PropertyToID("_NumberSteps");
+    private static readonly int TotalDepth = Shader.PropertyToID("_TotalDepth");
+    private static readonly int NoiseSize = Shader.PropertyToID("_NoiseSize");
+    private static readonly int NoiseSpeed = Shader.PropertyToID("_NoiseSpeed");
+    private static readonly int HueSize = Shader.PropertyToID("_HueSize");
+    private static readonly int BaseHue = Shader.PropertyToID("_BaseHue");
 
-    void Start()
+    #region Unity Events
+    
+    private void Start()
     {
         m_interactableObject = GetComponentInChildren<VRTK_InteractableObject>();
         m_interactableObject.InteractableObjectGrabbed += TurnOn;
         m_interactableObject.InteractableObjectUngrabbed += TurnOff;
 
+        m_controllerReference = VRTK_ControllerReference.GetControllerReference(VRTK_DeviceFinder.GetControllerRightHand(true));
+        
         m_renderers = Target.GetComponentsInChildren<Renderer>().ToList();
-        m_renderers.ForEach(r =>
+        m_renderers.ForEach(_r =>
         {
-            r.materials.ToList().ForEach(m =>
+            _r.materials.ToList().ForEach(_m =>
             {
-                m.shader = EffectBlend;
-                m.SetFloat("_EffectBlend", 0);
-                m.SetFloat("_NumberSteps", EffectParameters.GetFloat("_NumberSteps"));
-                m.SetFloat("_TotalDepth", EffectParameters.GetFloat("_TotalDepth"));
-                m.SetFloat("_NoiseSize", EffectParameters.GetFloat("_NoiseSize"));
-                m.SetFloat("_NoiseSpeed", EffectParameters.GetFloat("_NoiseSpeed"));
-                m.SetFloat("_HueSize", EffectParameters.GetFloat("_HueSize"));
-                m.SetFloat("_BaseHue", EffectParameters.GetFloat("_BaseHue"));
+                _m.shader = EffectBlend;
+                _m.SetFloat(Blend, 0);
+                _m.SetFloat(NumberSteps, EffectParameters.GetFloat(NumberSteps));
+                _m.SetFloat(TotalDepth, EffectParameters.GetFloat(TotalDepth));
+                _m.SetFloat(NoiseSize, EffectParameters.GetFloat(NoiseSize));
+                _m.SetFloat(NoiseSpeed, EffectParameters.GetFloat(NoiseSpeed));
+                _m.SetFloat(HueSize, EffectParameters.GetFloat(HueSize));
+                _m.SetFloat(BaseHue, EffectParameters.GetFloat(BaseHue));
             });
         });
     }
 
-    private void TurnOn(object sender, InteractableObjectEventArgs e)
+    private void Update()
     {
-        StartCoroutine(FadeEffect(true));
-        m_controllerReference = VRTK_ControllerReference.GetControllerReference(e.interactingObject);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_active = !m_active;
+            StartCoroutine(FadeEffect());
+            
+            if (m_active)
+                StartCoroutine(LoopHapticEffect());
+            else
+                CancelHapticEffect();
+        }
     }
 
-    private void TurnOff(object sender, InteractableObjectEventArgs e)
+    #endregion
+
+    #region Event Handling
+    
+    private void TurnOn(object _sender, InteractableObjectEventArgs _e)
     {
-        StartCoroutine(FadeEffect(false));
+        m_active = true;
+        m_controllerReference = VRTK_ControllerReference.GetControllerReference(_e.interactingObject);
+        StartCoroutine(FadeEffect());
+        StartCoroutine(LoopHapticEffect());
     }
 
-    IEnumerator FadeEffect(bool _state)
+    private void TurnOff(object _sender, InteractableObjectEventArgs _e)
     {
-        m_active = _state;
+        m_active = false;
+        StartCoroutine(FadeEffect());
+        CancelHapticEffect();
+    }
+
+    #endregion
+
+    #region Shader Effect
+
+    private IEnumerator FadeEffect()
+    {
+        bool startState = m_active;
         float start = Time.time;
-        while (Time.time < start + BlendDuration && m_active == _state)
+        while (Time.time < start + BlendDuration && m_active == startState)
         {
             float amount = (Time.time - start) / BlendDuration;
             ApplyEffect((m_active ? amount : 1 - amount) * EffectMax);
             yield return 1;
         }
-        if (m_active == _state)
+        if (m_active == startState)
             ApplyEffect(m_active ? 1 : 0 * EffectMax);
-    }
-
-    IEnumerator HapticEffect()
-    {
-        while (m_active)
-        {
-            VRTK_ControllerHaptics.TriggerHapticPulse(m_controllerReference, HapticClip);
-            yield return 1;
-        }
     }
 
     private void ApplyEffect(float _amount)
     {
-        m_renderers.ForEach(r =>
+        m_renderers.ForEach(_r =>
         {
-            r.materials.ToList().ForEach(m =>
+            _r.materials.ToList().ForEach(_m =>
             {
-                m.SetFloat("_EffectBlend", _amount);
+                _m.SetFloat(Blend, _amount);
             });
         });
     }
 
-    void Update()
+    #endregion
+
+    #region Haptic Effect
+
+    private IEnumerator LoopHapticEffect()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        while (m_active)
         {
-            m_active = !m_active;
-            StartCoroutine(FadeEffect(m_active));
+            VRTK_ControllerHaptics.TriggerHapticPulse(m_controllerReference, HapticClip);
+            yield return new WaitForSeconds(HapticClip.length);
         }
     }
+
+    private void CancelHapticEffect()
+    {
+        VRTK_ControllerHaptics.CancelHapticPulse(m_controllerReference);
+    }
+    
+
+    #endregion
 }
