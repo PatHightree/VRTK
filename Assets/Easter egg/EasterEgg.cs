0a9bb18e61c8ae213ee1b900ffee312b68082c6f
Patrick Hogenboom <patrickhightree@gmail.com>
2019-04-08 23:59:02 +0200

    Implemented easter egg trigger, plays haptic clip
    Hold controller upside down and rapidly click the trigger 3 times

--- Assets/Easter egg/EasterEgg.cs
+++ Assets/Easter egg/EasterEgg.cs
@@ -17,6 +17,12 @@ public class EasterEgg : MonoBehaviour
     private List<Renderer> m_renderers;
     private VRTK_InteractableObject m_interactableObject;
     private VRTK_ControllerReference m_controllerReference;
+    private VRTK_ControllerEvents m_controllerEvents;
+    private float m_lastClick;
+    private int m_clickCount;
+    private const int ClickThreshold = 3;
+    private const float ClickMaxDuration = 0.5f;
+    private const float UpsideDownThreshold = 90;
     private static readonly int Blend = Shader.PropertyToID("_EffectBlend");
     private static readonly int NumberSteps = Shader.PropertyToID("_NumberSteps");
     private static readonly int TotalDepth = Shader.PropertyToID("_TotalDepth");
@@ -30,10 +36,8 @@ public class EasterEgg : MonoBehaviour
     private void Start()
     {
         m_interactableObject = GetComponentInChildren<VRTK_InteractableObject>();
-        m_interactableObject.InteractableObjectGrabbed += TurnOn;
-        m_interactableObject.InteractableObjectUngrabbed += TurnOff;
-
-        m_controllerReference = VRTK_ControllerReference.GetControllerReference(VRTK_DeviceFinder.GetControllerRightHand(true));
+        m_interactableObject.InteractableObjectGrabbed += Grabbed;
+        m_interactableObject.InteractableObjectUngrabbed += Ungrabbed;
         
         m_renderers = Target.GetComponentsInChildren<Renderer>().ToList();
         m_renderers.ForEach(_r =>
@@ -50,39 +54,71 @@ public class EasterEgg : MonoBehaviour
                 _m.SetFloat(BaseHue, EffectParameters.GetFloat(BaseHue));
             });
         });
-    }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        VRTK_SDKManager.instance.LoadedSetupChanged += (_sender, _args) =>
         {
-            m_active = !m_active;
-            StartCoroutine(FadeEffect());
-            
-            if (m_active)
-                StartCoroutine(LoopHapticEffect());
-            else
-                CancelHapticEffect();
-        }
+            if (_sender.loadedSetup == null || !_sender.loadedSetup.isValid) return;
+            GameObject controllerRight = VRTK_DeviceFinder.GetControllerRightHand(true);
+            m_controllerReference = VRTK_ControllerReference.GetControllerReference(controllerRight);
+            m_controllerEvents = VRTK_DeviceFinder.GetScriptAliasController(controllerRight).GetComponent<VRTK_ControllerEvents>();
+            m_controllerEvents.TriggerPressed += TriggerPressed;
+        };
     }
 
     #endregion
 
     #region Event Handling
     
-    private void TurnOn(object _sender, InteractableObjectEventArgs _e)
+    private void Grabbed(object _sender, InteractableObjectEventArgs _e)
     {
-        m_active = true;
-        m_controllerReference = VRTK_ControllerReference.GetControllerReference(_e.interactingObject);
-        StartCoroutine(FadeEffect());
-        StartCoroutine(LoopHapticEffect());
+        // If player re-grabs while effect is active, start haptics immediately
+        if (m_active);
+            StartCoroutine(LoopHapticEffect(0));
     }
 
-    private void TurnOff(object _sender, InteractableObjectEventArgs _e)
+    private void Ungrabbed(object _sender, InteractableObjectEventArgs _e)
     {
-        m_active = false;
-        StartCoroutine(FadeEffect());
-        CancelHapticEffect();
+        // If player lets go of the controller while effect is active, cancel haptics immediately
+        if (!m_active)
+            StartCoroutine(CancelHapticEffect(0));
+    }
+
+    private void TriggerPressed(object _sender, ControllerInteractionEventArgs _e)
+    {
+        if (!m_interactableObject.IsGrabbed()) return;
+        
+        // To activate, the controller must be held upside down (no angle restriction for deactivating)
+        if (Vector3.Angle(_e.controllerReference.actual.transform.up, Vector3.up) < UpsideDownThreshold && !m_active)
+        {
+            Debug.LogFormat("<color=red>Angle {0}</color>", Vector3.Angle(_e.controllerReference.actual.transform.up, Vector3.up));
+            return;
+        }
+        
+        // To activate click 3 times in 1.5 seconds
+        if (Time.time < m_lastClick + ClickMaxDuration)
+        {
+            m_clickCount++;
+            Debug.LogFormat("<color=green>Clicked within time limit : {0}</color>", m_clickCount);
+            if (m_clickCount == ClickThreshold - 1)
+            {
+                Debug.LogFormat("<color=green>Clicked the threshold #</color>");
+                m_active = !m_active;
+                m_controllerReference = _e.controllerReference;
+                StartCoroutine(FadeEffect());
+                
+                // Start/stop haptics halfway through the effect fade
+                StartCoroutine(m_active
+                    ? LoopHapticEffect(BlendDuration / 2.0f)
+                    : CancelHapticEffect(BlendDuration / 2.0f));
+            }
+        }
+        else
+        {
+            Debug.LogFormat("<color=red>Clicked too late {0} > {1} + {2}</color>", Time.time, m_lastClick, ClickMaxDuration);
+            m_clickCount = 0;
+        }
+        m_lastClick = Time.time;
     }
 
     #endregion
@@ -118,20 +154,21 @@ public class EasterEgg : MonoBehaviour
 
     #region Haptic Effect
 
-    private IEnumerator LoopHapticEffect()
+    private IEnumerator LoopHapticEffect(float _delay)
     {
-        while (m_active)
+        yield return new WaitForSeconds(_delay);
+        while (m_active && m_interactableObject.IsGrabbed())
         {
             VRTK_ControllerHaptics.TriggerHapticPulse(m_controllerReference, HapticClip);
             yield return new WaitForSeconds(HapticClip.length);
         }
     }
 
-    private void CancelHapticEffect()
+    private IEnumerator CancelHapticEffect(float _delay)
     {
+        yield return new WaitForSeconds(_delay);
         VRTK_ControllerHaptics.CancelHapticPulse(m_controllerReference);
     }
-    
 
     #endregion
 }
