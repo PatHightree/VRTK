c43096d14b44104386c5a1354470addba6e18dfb
Patrick Hogenboom <patrickhightree@gmail.com>
2019-04-10 18:08:00 +0200

    Abstract class refactor, added more sound

--- Assets/Easter egg/EasterEgg.cs
+++ Assets/Easter egg/EasterEgg.cs
@@ -1,221 +1,108 @@
-﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using VRTK;
 
-public class EasterEgg : MonoBehaviour
+public enum EasterEggState
 {
-    public Transform Target;
-    public float FadeDuration = 2;
-    [Header("Visuals")] 
-    [Range(0, 1)]public float FadeIn;
-    public Shader EffectShader;
-    public Material EffectParameters;
+    PrerequisitesNotMet,
+    PrerequisitesMet,
+    SetUp,
+    SettingUp,
+    Activating,
+    Active,
+    Deactivating,
+    TearingDown
+}
 
-    [Header("Haptics")]
-    [Range(0, 1)] public float HapticsIntensity = 1;
-    [Range(0, 1)] public float OscillationResult;
-    [Range(0, 1)] public float OscillateMinStrength = 0.07f;
-    [Range(0, 1)] public float OscillateMaxStrength = 0.25f;
-    [Range(0, 1)] public float OscillatePeriod = 0.5f;
-    [Range(0, 1)] public float UpdateInterval = 0.05f;
-    [Range(0.00001f, 0.1f)] public float PulseInterval = 0.01f;
+public abstract  class EasterEgg : MonoBehaviour
+{
+    public EasterEggState State { get; private set; } = EasterEggState.PrerequisitesNotMet;
 
-    [Header("Audio")] 
-    public float AudioMin = 0.05f;
-    public float AudioMax = 0.15f;
+    protected Transform Target { get; private set; }
     
-    private bool m_active;
-    private List<Renderer> m_renderers;
-    private VRTK_InteractableObject m_interactableObject;
-    private VRTK_ControllerEvents m_controllerEvents;
-    private float m_lastClick;
-    private int m_clickCount;
-    private const int ClickThreshold = 3;
-    private const float ClickMaxDuration = 0.5f;
-    private const float UpsideDownThreshold = 90;
-    private Coroutine m_effectRoutine;
-    private AudioSource m_buzzSource;
-    private ParticleSystem m_confetti;
-    private float m_triggerPressure;
-    private static readonly int EffectBlend = Shader.PropertyToID("_EffectBlend");
-    private static readonly int Emission = Shader.PropertyToID("_Emission");
-    private static readonly int NumberSteps = Shader.PropertyToID("_NumberSteps");
-    private static readonly int TotalDepth = Shader.PropertyToID("_TotalDepth");
-    private static readonly int NoiseSize = Shader.PropertyToID("_NoiseSize");
-    private static readonly int NoiseSpeed = Shader.PropertyToID("_NoiseSpeed");
-    private static readonly int HueSize = Shader.PropertyToID("_HueSize");
-    private static readonly int BaseHue = Shader.PropertyToID("_BaseHue");
+    private List<Type> m_targetPrerequisites;
+    private List<Type> m_childPrerequisites;
 
-    #region Unity Events
+    protected void Create(Transform _target, List<Type> _targetPrerequisites, List<Type> _childPrerequisites)
+    {
+        Target = _target;
+        m_targetPrerequisites = _targetPrerequisites;
+        m_childPrerequisites = _childPrerequisites;
+        
+        CheckPrerequisites();
+    }
+
+    public bool CheckPrerequisites()
+    {
+        State = m_targetPrerequisites.All(_p => Target.GetComponent(_p) != null) && 
+                m_childPrerequisites.All(_p => Target.GetComponentInChildren(_p) != null)  
+            ? EasterEggState.PrerequisitesMet
+            : EasterEggState.PrerequisitesNotMet;
+        return State == EasterEggState.PrerequisitesMet;
+    }
     
-    private void Start()
+    public void Setup()
     {
-        m_interactableObject = GetComponentInChildren<VRTK_InteractableObject>();
-        m_interactableObject.InteractableObjectGrabbed += Grabbed;
-        m_interactableObject.InteractableObjectUngrabbed += Ungrabbed;
+        if (State != EasterEggState.PrerequisitesMet) return;
         
-        m_renderers = Target.GetComponentsInChildren<Renderer>().ToList();
-        m_renderers.ForEach(_r =>
-        {
-            _r.materials.ToList().ForEach(_m =>
-            {
-                _m.shader = EffectShader;
-                _m.SetFloat(EffectBlend, 0);
-                _m.SetFloat(NumberSteps, EffectParameters.GetFloat(NumberSteps));
-                _m.SetFloat(TotalDepth, EffectParameters.GetFloat(TotalDepth));
-                _m.SetFloat(NoiseSize, EffectParameters.GetFloat(NoiseSize));
-                _m.SetFloat(NoiseSpeed, EffectParameters.GetFloat(NoiseSpeed));
-                _m.SetFloat(HueSize, EffectParameters.GetFloat(HueSize));
-                _m.SetFloat(BaseHue, EffectParameters.GetFloat(BaseHue));
-            });
-        });
-
-        m_buzzSource = GetComponent<AudioSource>();
-        m_buzzSource.volume = 0;
-
-        m_confetti = GetComponentInChildren<ParticleSystem>();
-
-        VRTK_SDKManager.instance.LoadedSetupChanged += (_sender, _args) =>
-        {
-            if (_sender.loadedSetup == null || !_sender.loadedSetup.isValid) return;
-            GameObject controllerRight = VRTK_DeviceFinder.GetControllerRightHand(true);
-            m_controllerEvents = VRTK_DeviceFinder.GetScriptAliasController(controllerRight).GetComponent<VRTK_ControllerEvents>();
-            m_controllerEvents.TriggerPressed += TriggerPressed;
-            m_controllerEvents.TriggerReleased += TriggerReleased;
-            m_controllerEvents.TriggerAxisChanged += TriggerAxisChanged; 
-        };
+        State = EasterEggState.SettingUp;
+        DoSetup();
+        State = EasterEggState.SetUp;
     }
+    protected abstract void DoSetup();
 
-    #endregion
-
-    #region Event Handling
+    protected void CheckActivationTrigger(object _sender)
+    {
+        if (State != EasterEggState.SetUp) return;
+        if (!DoCheckActivationTrigger(_sender)) return;
+        
+        Activate();
+    }
+    protected abstract bool DoCheckActivationTrigger(object _sender);
     
-    private void Grabbed(object _sender, InteractableObjectEventArgs _e)
+    protected void CheckDeactivationTrigger(object _sender)
     {
-        // If player re-grabs while effect is active, start haptics immediately
-        if (m_active)
-            HapticsIntensity = 1;
-    }
-
-    private void Ungrabbed(object _sender, InteractableObjectEventArgs _e)
-    {
-        // If player lets go of the controller while effect is active, cancel haptics immediately
-        if (!m_active)
-            HapticsIntensity = 0;
-    }
-
-    private void TriggerPressed(object _sender, ControllerInteractionEventArgs _e)
-    {
-        if (!m_interactableObject.IsGrabbed()) return;
+        if (State != EasterEggState.Active) return;
+        if (!DoCheckDeactivationTrigger(_sender)) return;
         
-        if (m_active)
-            m_confetti.Play();
+        Deactivate();
+    }
+    protected abstract bool DoCheckDeactivationTrigger(object _sender);
+    
+    public void Activate()
+    {
+        if (State != EasterEggState.SetUp) return;
         
-        // To (de)activate, the controller must be held upside down
-        if (Vector3.Angle(_e.controllerReference.actual.transform.up, Vector3.up) < UpsideDownThreshold)
-        {
-            Debug.LogFormat("<color=red>Angle {0}</color>", Vector3.Angle(_e.controllerReference.actual.transform.up, Vector3.up));
-            return;
-        }
+        State = EasterEggState.Activating;
+        DoActivate();
+    }
+    protected abstract void DoActivate();
+    protected void DoActivateFinished()
+    {
+        State = EasterEggState.Active;
+    }
+
+    public void Deactivate()
+    {
+        if (State != EasterEggState.Active) return;
         
-        // To (de)activate click 3 times in 1.5 seconds
-        if (Time.time < m_lastClick + ClickMaxDuration)
-        {
-            m_clickCount++;
-            Debug.LogFormat("<color=green>Clicked within time limit : {0}</color>", m_clickCount);
-            if (m_clickCount == ClickThreshold - 1)
-            {
-                m_active = !m_active;
-                Debug.LogFormat("<color=green>Clicked the threshold # active={0}</color>", m_active);
-                StartCoroutine(Fade());
-                m_effectRoutine = StartCoroutine(Effect());
-            }
-        }
-        else
-        {
-            Debug.LogFormat("<color=red>Clicked too late {0} > {1} + {2}</color>", Time.time, m_lastClick, ClickMaxDuration);
-            m_clickCount = 0;
-        }
-        m_lastClick = Time.time;
+        State = EasterEggState.Deactivating;
+        DoDeactivate();
     }
-
-    private void TriggerAxisChanged(object _sender, ControllerInteractionEventArgs _e)
+    protected abstract void DoDeactivate();
+    protected void DoDeactivateFinished()
     {
-        m_triggerPressure = _e.buttonPressure;
+        State = EasterEggState.SetUp;
     }
 
-    private void TriggerReleased(object _sender, ControllerInteractionEventArgs _e)
+    public void Teardown()
     {
-        m_confetti.Stop();
+        if (State != EasterEggState.SetUp) return;
+
+        State = EasterEggState.TearingDown;
+        DoTeardown();
+        State = EasterEggState.PrerequisitesMet;
     }
-
-    #endregion
-
-    #region Shader Effect
-
-    private IEnumerator Fade()
-    {
-        bool startState = m_active;
-        float start = Time.time;
-        while (Time.time < start + FadeDuration && m_active == startState)
-        {
-            FadeIn = (Time.time - start) / FadeDuration;
-            FadeIn = (m_active ? FadeIn : 1 - FadeIn);
-            yield return 1;
-        }
-
-        if (m_active == startState)
-        {
-            FadeIn = m_active ? 1 : 0;
-            if (!m_active)
-                StopCoroutine(m_effectRoutine);
-        }        
-    }
-
-    private void ApplyShaderEffect(float _amount)
-    {
-        m_renderers.ForEach(_r =>
-        {
-            _r.materials.ToList().ForEach(_m =>
-            {
-                _m.SetFloat(EffectBlend, _amount);
-                _m.SetFloat(Emission, m_triggerPressure * 0.5f);
-            });
-        });
-    }
-
-    #endregion
-
-    private IEnumerator Effect()
-    {
-        while (true)
-        {
-            // Oscillation
-            float oscillation = Mathf.Sin(Time.time / OscillatePeriod / 2 * Mathf.PI) / 2.0f + 0.5f;
-            OscillationResult = Mathf.Lerp(OscillateMinStrength, OscillateMaxStrength, oscillation);
-            float effectStrength = Mathf.Max(oscillation, m_triggerPressure); 
-            
-            // Haptics
-            HapticsIntensity = FadeIn * effectStrength;
-            if (m_interactableObject.IsGrabbed())
-                VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerReferenceRightHand(), HapticsIntensity, UpdateInterval, PulseInterval);
-            else
-                VRTK_ControllerHaptics.CancelHapticPulse(VRTK_DeviceFinder.GetControllerReferenceRightHand());
-                
-            // Visuals
-            ApplyShaderEffect(Mathf.Lerp(0.5f, 1, effectStrength) * FadeIn);
-            
-            // Audio
-            float audioMaxBoost = Mathf.Lerp(AudioMax, 2, m_triggerPressure);
-            m_buzzSource.volume = Mathf.Lerp(AudioMin, audioMaxBoost, Mathf.Max(effectStrength, m_triggerPressure)) * FadeIn;
-
-            // Confetti speed
-            ParticleSystem.MainModule main = m_confetti.main;
-            main.startSpeedMultiplier = m_triggerPressure * 3;
-
-            yield return new WaitForSeconds(UpdateInterval + PulseInterval);
-        }
-    }
+    protected abstract void DoTeardown();
 }
---
+++ Assets/Easter egg/EasterEggMagicAkron.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VRTK;
+
+public class EasterEggMagicAkron : EasterEgg
+{
+    public float FadeDuration = 2;
+    [Header("Visuals")] 
+    [Range(0, 1)]public float FadeAmount;
+    public Shader EffectShader;
+    public Material EffectParameters;
+
+    [Header("Haptics")]
+    [Range(0, 1)] public float HapticsIntensity = 1;
+    [Range(0, 1)] public float OscillationResult;
+    [Range(0, 1)] public float OscillateMinStrength = 0.07f;
+    [Range(0, 1)] public float OscillateMaxStrength = 0.25f;
+    [Range(0, 1)] public float OscillatePeriod = 0.5f;
+    [Range(0, 1)] public float UpdateInterval = 0.05f;
+    [Range(0.00001f, 0.1f)] public float PulseInterval = 0.01f;
+
+    [Header("Audio")] 
+    public float HumMinVolume = 0.05f;
+    public float HumMaxVolume = 0.15f;
+    public AudioClip Hum;
+    public float RushMinVolume = 0.05f;
+    public float RushMaxVolume = 0.15f;
+    public AudioClip Rush;
+    
+    private List<Renderer> m_renderers;
+    private VRTK_InteractableObject m_interactableObject;
+    private VRTK_ControllerEvents m_controllerEvents;
+    private float m_lastClick;
+    private int m_clickCount;
+    private const int ClickThreshold = 3;
+    private const float ClickMaxDuration = 0.5f;
+    private const float UpsideDownThreshold = 90;
+    private Coroutine m_effectRoutine;
+    private AudioSource m_humSource;
+    private AudioSource m_rushSource;
+    private ParticleSystem m_confetti;
+    private float m_triggerPressure;
+    private static readonly int EffectBlend = Shader.PropertyToID("_EffectBlend");
+    private static readonly int Emission = Shader.PropertyToID("_Emission");
+    private static readonly int NumberSteps = Shader.PropertyToID("_NumberSteps");
+    private static readonly int TotalDepth = Shader.PropertyToID("_TotalDepth");
+    private static readonly int NoiseSize = Shader.PropertyToID("_NoiseSize");
+    private static readonly int NoiseSpeed = Shader.PropertyToID("_NoiseSpeed");
+    private static readonly int HueSize = Shader.PropertyToID("_HueSize");
+    private static readonly int BaseHue = Shader.PropertyToID("_BaseHue");
+
+    
+    private void Awake()
+    {
+        Create(transform, 
+            new List<Type> {typeof(VRTK_InteractableObject), typeof(AudioSource)}, 
+            new List<Type> { typeof(ParticleSystem) });
+    }
+
+    private void Start()
+    {
+        Setup();
+    }
+    
+    protected override void DoSetup()
+    {
+        m_interactableObject = Target.GetComponentInChildren<VRTK_InteractableObject>();
+        m_interactableObject.InteractableObjectGrabbed += Grabbed;
+        m_interactableObject.InteractableObjectUngrabbed += Ungrabbed;
+        
+        m_confetti = Target.GetComponentInChildren<ParticleSystem>();
+
+        m_renderers = Target.GetComponentsInChildren<Renderer>().ToList();
+        m_renderers.ForEach(_r =>
+        {
+            if (_r.gameObject == m_confetti.gameObject) return;
+            
+            _r.materials.ToList().ForEach(_m =>
+            {
+                _m.shader = EffectShader;
+                _m.SetFloat(EffectBlend, 0);
+                _m.SetFloat(NumberSteps, EffectParameters.GetFloat(NumberSteps));
+                _m.SetFloat(TotalDepth, EffectParameters.GetFloat(TotalDepth));
+                _m.SetFloat(NoiseSize, EffectParameters.GetFloat(NoiseSize));
+                _m.SetFloat(NoiseSpeed, EffectParameters.GetFloat(NoiseSpeed));
+                _m.SetFloat(HueSize, EffectParameters.GetFloat(HueSize));
+                _m.SetFloat(BaseHue, EffectParameters.GetFloat(BaseHue));
+            });
+        });
+
+        m_humSource = Target.gameObject.AddComponent<AudioSource>();
+        m_humSource.clip = Hum;
+        m_humSource.volume = 0;
+        m_humSource.loop = true;
+        m_humSource.spatialBlend = 1;
+        m_humSource.Play();
+        m_rushSource = Target.gameObject.AddComponent<AudioSource>();
+        m_rushSource.clip = Rush;
+        m_rushSource.volume = 0;
+        m_rushSource.loop = true;
+        m_rushSource.spatialBlend = 1;
+        m_rushSource.Play();
+
+        VRTK_SDKManager.instance.LoadedSetupChanged += (_sender, _args) =>
+        {
+            if (_sender.loadedSetup == null || !_sender.loadedSetup.isValid) return;
+            GameObject controllerRight = VRTK_DeviceFinder.GetControllerRightHand(true);
+            m_controllerEvents = VRTK_DeviceFinder.GetScriptAliasController(controllerRight).GetComponent<VRTK_ControllerEvents>();
+            m_controllerEvents.TriggerPressed += TriggerPressed;
+            m_controllerEvents.TriggerReleased += TriggerReleased;
+            m_controllerEvents.TriggerAxisChanged += TriggerAxisChanged; 
+        };
+    }
+
+    protected override bool DoCheckActivationTrigger(object _sender)
+    {
+        if (!(_sender is VRTK_ControllerReference controllerReference)) return false;
+        if (!m_interactableObject.IsGrabbed()) return false;
+        
+        // To (de)activate, the controller must be held upside down
+        if (Vector3.Angle(controllerReference.actual.transform.up, Vector3.up) < UpsideDownThreshold)
+        {
+            Debug.LogFormat("<color=red>Angle {0}</color>", Vector3.Angle(controllerReference.actual.transform.up, Vector3.up));
+            return false;
+        }
+        
+        // To (de)activate click 3 times in 1.5 seconds
+        if (Time.time < m_lastClick + ClickMaxDuration)
+        {
+            m_clickCount++;
+            Debug.LogFormat("<color=green>Clicked within time limit : {0}</color>", m_clickCount);
+            if (m_clickCount == ClickThreshold - 1)
+            {
+                Debug.LogFormat("<color=green>Clicked the threshold # : Activating</color>");
+                
+                return true;
+            }
+        }
+        else
+        {
+            Debug.LogFormat("<color=red>Clicked too late {0} > {1} + {2}</color>", Time.time, m_lastClick, ClickMaxDuration);
+            m_clickCount = 0;
+        }
+        
+        m_lastClick = Time.time;
+        return false;
+    }
+
+    protected override bool DoCheckDeactivationTrigger(object _sender)
+    {
+        return DoCheckActivationTrigger(_sender);
+    }
+
+    protected override void DoActivate()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    protected override void DoDeactivate()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    protected override void DoTeardown()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #region Event Handling
+    
+    private void Grabbed(object _sender, InteractableObjectEventArgs _e)
+    {
+        // If player re-grabs while effect is active, start haptics immediately
+        if (State == EasterEggState.Active)
+            HapticsIntensity = 1;
+    }
+
+    private void Ungrabbed(object _sender, InteractableObjectEventArgs _e)
+    {
+        // If player lets go of the controller while effect is active, cancel haptics immediately
+        if (State == EasterEggState.Active)
+            HapticsIntensity = 0;
+    }
+
+    private void TriggerPressed(object _sender, ControllerInteractionEventArgs _e)
+    {
+        CheckActivationTrigger(_e.controllerReference);
+        CheckDeactivationTrigger(_e.controllerReference);
+        
+        if (State == EasterEggState.Active)
+            m_confetti.Play();
+    }
+
+    private void TriggerAxisChanged(object _sender, ControllerInteractionEventArgs _e)
+    {
+        m_triggerPressure = _e.buttonPressure;
+    }
+
+    private void TriggerReleased(object _sender, ControllerInteractionEventArgs _e)
+    {
+        m_confetti.Stop();
+    }
+
+    #endregion
+
+    #region Shader Effect
+
+    private IEnumerator FadeIn()
+    {
+        // Start effect coroutine
+        FadeAmount = 0;
+        m_effectRoutine = StartCoroutine(Effect());
+        
+        // Animate FadeAmount
+        float start = Time.time;
+        while (Time.time < start + FadeDuration)
+        {
+            FadeAmount = (Time.time - start) / FadeDuration;
+            yield return 1;
+        }
+        FadeAmount = 1;
+        
+        // Signal that activation is complete
+        DoActivateFinished();
+    }
+
+    private IEnumerator FadeOut()
+    {
+        // Animate FadeAmount
+        float start = Time.time;
+        while (Time.time < start + FadeDuration)
+        {
+            FadeAmount = 1 - ((Time.time - start) / FadeDuration);
+            yield return 1;
+        }
+        FadeAmount = 0;
+        yield return 1;
+        
+        // Stop effect coroutine
+        StopCoroutine(m_effectRoutine);
+        
+        // Signal that deactivation is complete
+        DoDeactivateFinished();
+    }
+
+    private void ApplyShaderEffect(float _amount)
+    {
+        m_renderers.ForEach(_r =>
+        {
+            _r.materials.ToList().ForEach(_m =>
+            {
+                _m.SetFloat(EffectBlend, _amount);
+                _m.SetFloat(Emission, m_triggerPressure * 0.5f);
+            });
+        });
+    }
+
+    #endregion
+
+    private IEnumerator Effect()
+    {
+        while (true)
+        {
+            // Oscillation
+            float oscillation = Mathf.Sin(Time.time / OscillatePeriod / 2 * Mathf.PI) / 2.0f + 0.5f;
+            OscillationResult = Mathf.Lerp(OscillateMinStrength, OscillateMaxStrength, oscillation);
+            float effectStrength = Mathf.Max(oscillation, m_triggerPressure); 
+            
+            // Haptics
+            HapticsIntensity = FadeAmount * effectStrength;
+            if (m_interactableObject.IsGrabbed())
+                VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerReferenceRightHand(), HapticsIntensity, UpdateInterval, PulseInterval);
+            else
+                VRTK_ControllerHaptics.CancelHapticPulse(VRTK_DeviceFinder.GetControllerReferenceRightHand());
+                
+            // Visuals
+            ApplyShaderEffect(Mathf.Lerp(0.5f, 1, effectStrength) * FadeAmount);
+            
+            // Audio
+            m_humSource.volume = Mathf.Lerp(HumMinVolume, HumMaxVolume, effectStrength) * FadeAmount;
+
+//            float audioMaxBoost = Mathf.Lerp(HumMaxVolume, 2, m_triggerPressure);
+            m_rushSource.volume = Mathf.Lerp(RushMinVolume, RushMaxVolume, m_triggerPressure) * FadeAmount;
+
+            // Confetti speed
+            ParticleSystem.MainModule main = m_confetti.main;
+            main.startSpeedMultiplier = m_triggerPressure * 3;
+
+            yield return new WaitForSeconds(UpdateInterval + PulseInterval);
+        }
+    }
+
+}
